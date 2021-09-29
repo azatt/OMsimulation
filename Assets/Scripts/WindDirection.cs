912dfef6fce6edd84f1f9dc59c9707efb01baa0d
Azat <azatsa@gmail.com>
2021-09-29 10:34:08 +0200

    Initial setup

---
+++ Assets/Scripts/WindDirection.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindDirection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Random.rotation;
+    }
+
+    // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WindForce.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindForce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Rigidbody>().AddForce(transform.forward * 10);
+    }
+}
