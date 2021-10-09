9699d660ff34f7046d9ddfd16ca532d537f33520
Azat <azatsa@gmail.com>
2021-10-09 12:46:00 +0200

    Human cleans up tumbleweed

--- Assets/Scripts/LifespanHuman.cs
+++ Assets/Scripts/LifespanHuman.cs
@@ -10,19 +10,23 @@ public class LifespanHuman : MonoBehaviour
     private float translate;
     private Vector3 targetPosition;
 
+    private bool foundTumbleweed;
+    private GameObject foundTumbleweedObject;
+
     // Start is called before the first frame update
     void Start()
     {
         timeObject = GameObject.FindWithTag("time");
         timeM = timeObject.GetComponent<TimeMultipier>();
         targetPosition = new Vector3(planeRange * Random.value, transform.position.y, planeRange * Random.value);
+        foundTumbleweed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        WalkAround();
-
+        if (!foundTumbleweed) { WalkAround(); }
+        else { KillTumbleweed(); }
     }
 
     void WalkAround()
@@ -40,10 +44,24 @@ public class LifespanHuman : MonoBehaviour
         if (collider.tag == "tumbleweed")
         {
             Debug.Log("I found a tumbleweed!");
+            foundTumbleweed = true;
+            foundTumbleweedObject = collider.gameObject;
         }
     }
     private void KillTumbleweed()
     {
+        if (!foundTumbleweedObject.activeSelf)
+        {
+            foundTumbleweed = false;
+            return;
+        }
+        if (Vector3.Distance(foundTumbleweedObject.transform.position, transform.position) < 1)
+        {
+            SimplePool.Despawn(foundTumbleweedObject);
+            foundTumbleweed = false;
+            return;
+        }
 
+        transform.position = Vector3.MoveTowards(transform.position, foundTumbleweedObject.transform.position, translate);
     }
 }
